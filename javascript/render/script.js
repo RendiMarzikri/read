25fb310e2c31d97056a5f910ba0e59082466f955
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-07 16:32:03 +0700

    Fix link in webcontent to point to correct route

--- javascript/render/script.js
+++ javascript/render/script.js
@@ -1,6 +1,6 @@
 const webcontent = [
   {
-    "content": `<div class="z-container z-mt-3"><div class="z-card z-fade-in"><h2 class="z-fs-2">ブログや記事を読む</h2><p class="z-text-muted z-mt-1">下のボタンを押して読んでください。</p><a href="/read/blog/" class="z-btn z-btn-primary z-mt-2">読む</a></div></div>`
+    "content": `<div class="z-container z-mt-3"><div class="z-card z-fade-in"><h2 class="z-fs-2">ブログや記事を読む</h2><p class="z-text-muted z-mt-1">下のボタンを押して読んでください。</p><a href="/read/read/" class="z-btn z-btn-primary z-mt-2">読む</a></div></div>`
   }
 ];
 render_text(webcontent, "history");
