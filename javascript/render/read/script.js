eb7d7bbd155ac36c21a80f77049bdfea5290cc70
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-07 16:46:38 +0700

    Add script.js to render web content

---
+++ javascript/render/read/script.js
@@ -0,0 +1,13 @@
+const webcontent = [
+  {
+    "content": `
+    <div class="z-container z-mt-3">
+      <div class="z-card z-fade-in">
+        <h2 class="z-fs-2">記事を読む</h2>
+        <p class="z-text-muted z-mt-1">記事を読むには下のボタンを押してください。</p>
+        <a href="/read/read/article" class="z-btn z-btn-primary z-mt-2">記事</a>
+      </div>
+    </div>`
+  }
+];
+render_text(webcontent, "content");
