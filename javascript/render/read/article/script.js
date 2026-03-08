498438e192006b473e86f301dcf403af504f8474
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-08 20:24:38 +0700

    Add script.js for rendering article content

---
+++ javascript/render/read/article/script.js
@@ -0,0 +1,12 @@
+const webcontent = [
+  {
+    "content": `
+    <div class="z-container z-mt-3">
+      <div class="z-card z-fade-in">
+        <h2 class="z-fs-2">時を待つ...</h2>
+        <p class="z-text-muted z-mt-1">時を待つ...</p>
+     </div>
+    </div>`
+  }
+];
+render_text(webcontent, "content");
