e72d1fb53ac95b884f1d1abac103c0cec9ce638a
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-10 11:51:45 +0700

    Update article content and heading in script.js

--- javascript/render/read/article/script.js
+++ javascript/render/read/article/script.js
@@ -3,8 +3,8 @@ const webcontent = [
     "content": `
     <div class="z-container z-mt-3">
       <div class="z-card z-fade-in">
-        <h2 class="z-fs-2">時を待つ...</h2>
-        <p class="z-text-muted z-mt-1">時を待つ...</p>
+        <h2 class="z-fs-2">静かな朝</h2>
+        <p class="z-text-muted z-mt-1">ときどき、朝はとても静かに感じられる。空気はまだ涼しく、道はまだ混んでおらず、人々はちょうど一日の活動を始めたところだ。何人かの人は、忙しい一日を始める前に、少しの間座って静かにその雰囲気を楽しぶことを選ぶ。</p>
      </div>
     </div>`
   }
