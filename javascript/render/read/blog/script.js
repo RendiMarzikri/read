9cacdce24e24483ddd1c0a326e1e7a0be56976f9
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-12 04:40:17 +0700

    Add initial blog script with update message

---
+++ javascript/render/read/blog/script.js
@@ -0,0 +1,12 @@
+const webcontent = [
+  {
+    "content": `
+    <div class="z-container z-mt-3">
+      <div class="z-card z-fade-in">
+        <h2 class="z-fs-2">アップデートを待っています...</h2>
+        <p class="z-text-muted z-mt-1">アップデートを待っています...</p>
+     </div>
+    </div>`
+  }
+];
+render_text(webcontent, "content");
