3a02a409d1e92915cc44cb8a8b2c0ed05eb4a93a
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-14 05:11:32 +0700

    Add new content sections to webcontent array

--- javascript/render/script.js
+++ javascript/render/script.js
@@ -1,6 +1,9 @@
 const webcontent = [
   {
     "content": `<div class="z-container z-mt-3"><div class="z-card z-fade-in"><h2 class="z-fs-2">ブログや記事を読む</h2><p class="z-text-muted z-mt-1">下のボタンを押して読んでください。</p><a href="/read/read/" class="z-btn z-btn-primary z-mt-2">読む</a></div></div>`
+  },
+  {
+    "content": `<div class="z-container z-mt-3"><div class="z-card z-fade-in"><h2 class="z-fs-2">面白い話や重要な話を読んでみよう</h2><p class="z-text-muted z-mt-1">下のボタンを押すと、面白い話や重要な話を読むことができます。</p><a href="/read/read/story" class="z-btn z-btn-primary z-mt-2">読む</a></div></div>`
   }
 ];
 render_text(webcontent, "history");
