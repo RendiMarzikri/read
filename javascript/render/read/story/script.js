8af65ea7a5d69f130c2a1beaa4d738e78f383519
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-15 04:25:50 +0700

    Add script.js for rendering story content

---
+++ javascript/render/read/story/script.js
@@ -0,0 +1,13 @@
+const webcontent = [
+  {
+    "content": `
+    <div class="z-container z-mt-3">
+      <div class="z-card z-fade-in">
+        <h2 class="z-fs-2">帝国を題材にした架空の物語</h2>
+        <p class="z-text-muted z-mt-1">下のボタンを押すと、その帝国を題材にした架空の物語が読めます。</p>
+        <a href="/read/strory/emperor" class="z-btn z-btn-primary z-mt-2">読む</a>
+      </div>
+    </div>`
+  }
+];
+render_text(webcontent, "content");
