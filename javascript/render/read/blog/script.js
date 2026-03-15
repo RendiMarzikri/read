2b51a4be297b713cc4f5f169d9564c9fc0f7761a
Rendi Marzikri <arternosrendi@gmail.com>
2026-03-16 04:38:06 +0700

    Update blog section with news and YouTube link

--- javascript/render/read/blog/script.js
+++ javascript/render/read/blog/script.js
@@ -3,8 +3,9 @@ const webcontent = [
     "content": `
     <div class="z-container z-mt-3">
       <div class="z-card z-fade-in">
-        <h2 class="z-fs-2">アップデートを待っています...</h2>
-        <p class="z-text-muted z-mt-1">アップデートを待っています...</p>
+        <h2 class="z-fs-2">ニュース</h2>
+        <p class="z-text-muted z-mt-1">「Rendi Marzikri」の最新情報を入手するには、「Rendi Marzikri」のYouTubeチャンネルを購読してください。</p>
+        <a href="https://m.youtube.com/@User-bs1bc7sx5" class="z-btn z-btn-primary z-mt-2">学ぶ</a>
      </div>
     </div>`
   }
