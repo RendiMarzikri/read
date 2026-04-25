5ff5737f08bbc1b2f8ace40c9386113f93a3f3e8
Rendi Marzikri <arternosrendi@gmail.com>
2026-04-26 06:09:30 +0700

    Fix typo in story link URL

--- javascript/render/read/story/script.js
+++ javascript/render/read/story/script.js
@@ -5,7 +5,7 @@ const webcontent = [
       <div class="z-card z-fade-in">
         <h2 class="z-fs-2">帝国を題材にした架空の物語</h2>
         <p class="z-text-muted z-mt-1">下のボタンを押すと、その帝国を題材にした架空の物語が読めます。</p>
-        <a href="/read/read/strory/emperor" class="z-btn z-btn-primary z-mt-2">読む</a>
+        <a href="/read/read/story/emperor" class="z-btn z-btn-primary z-mt-2">読む</a>
       </div>
     </div>`
   }
