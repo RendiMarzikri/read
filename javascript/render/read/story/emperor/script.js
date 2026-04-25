ed079d71a8b641eed6a26d3b5009b363f4248ff0
Rendi Marzikri <arternosrendi@gmail.com>
2026-04-26 06:12:11 +0700

    Add script.js to render web content

---
+++ javascript/render/read/story/emperor/script.js
@@ -0,0 +1,12 @@
+const webcontent = [
+  {
+    "content": `
+    <div class="z-container z-mt-3">
+      <div class="z-card z-fade-in">
+        <h2 class="z-fs-2">待って...</h2>
+        <p class="z-text-muted z-mt-1">待って...</p>
+      </div>
+    </div>`
+  }
+];
+render_text(webcontent, "content");
